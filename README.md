da166356329c0f7fbee237f59cc43bbd6370ddd0
Igor Teixeira <igor.teixeira@cromai.com>
2022-05-19 16:48:25 -0300

    Refact: Organize code and add readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # readme-generator
 A readme generator based in markdown templates.
+
+## How to use
+```bash
+python readme_generator\generate.py
+``` 
---
+++ readme_generator/generate.py
@@ -0,0 +1,28 @@
+DATA = {
+    "$SERVICE": "",
+    "$SHORT_DESCRIPTION": '',
+    "$WHAT_IT_DOES": '',
+    "$FUNCTION_NAME": '',
+    "$HTTP_METHOD": ''
+}
+
+
+def generate_readme():
+    for key, value in DATA.items():
+        DATA[key] = input(f"Digite o {key[1:]}: ")
+
+    # Read in the file
+    with open('GCF_TEMPLATE.md', 'r', encoding='utf-8') as file:
+        filedata = file.read()
+
+    # Replace the target string
+    for k, v in DATA.items():
+        filedata = filedata.replace(k, v)
+
+    # Write the file out again
+    with open('README.md', 'w', encoding='utf-8') as file:
+        file.write(filedata)
+
+
+if __name__ == '__main__':
+    generate_readme()
